3222b1c063a14686d246998aefde36daaf2c40f1
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-01-31 15:59:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -21,14 +21,14 @@ class Solution {
         int ans = 0;
         
         for(int i =1; i <= n ; i++){
-            for(int j =1 ; j <= m ; j++){
+          for(int j =1 ; j <= m ; j++){
                 
-                if(s1[i-1] == s2[j-1]) {
+           if(s1[i-1] == s2[j-1]) { // we were doing shifting thats why we take i-1 and j-1
                     dp[i][j] = 1 + dp[i-1][j-1];
                     ans = max(ans , dp[i][j]);
                 }
                 else dp[i][j] = 0;
-            }
+           }
         }
         return ans;
     }
