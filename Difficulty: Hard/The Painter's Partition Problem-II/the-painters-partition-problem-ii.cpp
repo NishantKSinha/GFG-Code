d50d57017e53c346b131a63746ded985190de4c4
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-04-18 11:05:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -1,71 +1,75 @@
 //{ Driver Code Starts
-// driver code
-
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-//User function template for C++
-long long check(int arr[],long long mid,int n){
-    long long paint = 1;
-    long long sum = 0 ;
-    for(int i = 0 ; i < n;i++){
-        if(sum + arr[i] <= mid) sum = sum + arr[i];
-        else{
-            paint++;
-            sum = arr[i];
-        }
-    }
-    return paint;
-}
-class Solution
-{
+
+// User function template for C++
+
+class Solution {
   public:
-    long long minTime(int arr[], int n, int k)
-    {
-     long long max = -1;
-     for(int i = 0 ; i < n ; i++){
-         if(max < arr[i]) max = arr[i];
-     }
-     long long low = max;
-     
-    long long sum = 0;
-     for(int i = 0 ; i < n ; i++){
-        sum = sum + arr[i];
-     }
-     long long high = sum;
-    
-    while(low<=high){
-        long long mid = (low+high)/2;
-        long long count = check(arr,mid,n);
-        if(count > k) low = mid+1;
-        else{
-          
-           high = mid - 1;
+   bool countStudents(vector<int> &arr, int pages,int k ) {
+    int n = arr.size(); //size of array.
+    int students = 1;
+    long long pagesStudent = 0;
+    for (int i = 0; i < n; i++) {
+        if (pagesStudent + arr[i] <= pages) {
+            //add pages to current student
+            pagesStudent += arr[i];
+        }
+        else {
+            //add pages to next student
+            students++;
+            pagesStudent = arr[i];
         }
     }
-    return low;
+    if(students <= k) return true;  //catch point
+    else return false;
+}
+    int minTime(vector<int>& arr, int k) {
+        int low = *max_element(arr.begin(), arr.end());
+    int high = accumulate(arr.begin(), arr.end(), 0);
+     int ans =-1; 
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        
+        if ( countStudents(arr, mid,k) == true) {
+            ans = mid;
+            high = mid -1 ;
+        }
+        else {
+            low = mid +1 ;
+        }
+    }
+    return ans;
     }
 };
 
+
 //{ Driver Code Starts.
 
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		int k,n;
-		cin>>k>>n;
-		
-		int arr[n];
-		for(int i=0;i<n;i++)
-		    cin>>arr[i];
-		Solution obj;
-		cout << obj.minTime(arr, n, k) << endl;
-	}
-	return 0;
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        Solution obj;
+        cout << obj.minTime(arr, k) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
 }
 // } Driver Code Ends
