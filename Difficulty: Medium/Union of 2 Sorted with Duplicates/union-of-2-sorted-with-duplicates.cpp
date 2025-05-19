8984af89e6eeac910b540d45fd758e90699f3663
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-05-19 16:21:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.cpp
+++ Difficulty: Medium/Union of 2 Sorted with Duplicates/union-of-2-sorted-with-duplicates.cpp
@@ -12,53 +12,50 @@ class Solution {
     // Function to return a list containing the union of the two arrays.
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
         // Your code here
-        // return vector with correct order of elements
+        int i = 0 ,j = 0;
         vector<int>ans;
-        int i = 0, j = 0;
-        int n1 = a.size();
-        int n2 = b.size();
-        if(a[i] == b[j])
-        {
-            ans.push_back(a[i]);
-            i++;
-            j++;
-        }
-         else if(a[i] < b[j] ){
+        while(i < a.size() && j < b.size()){
+            
+            if(a[i] < b[j] ){
+                if(ans.empty() || ans.back() != a[i] ){
+                   ans.push_back(a[i]);
+                   i++;
+                }
+               else i++;
+            }
+            else if(b[j] < a[i] ){
+              if(ans.empty() || ans.back() != b[j] ){
+                   ans.push_back(b[j]);
+                   j++;
+                }
+               else j++;
+            }
+            else {
+                 if(ans.empty() || ans.back() != a[i] ){
                 ans.push_back(a[i]);
                 i++;
+                j++;
+                }
+                else{
+                    i++;
+                    j++;
+                }
             }
-         else{
+        }
+        while(i < a.size()){
+             if(ans.empty() || ans.back() != a[i] ){
+                 ans.push_back(a[i]);
+                   i++;
+             }
+             else i++;
+        }
+        while(j < b.size()){
+            if(ans.empty() || ans.back() != b[j] ){
                 ans.push_back(b[j]);
-                j++;
-            }
-        
-        while(i < n1 && j < n2){
-            if(a[i] == b[j] ){
-            if(ans.back() != a[i]) ans.push_back(a[i]);
-            i++;
             j++;
-        }
-            
-           else if(a[i] < b[j] ){
-               if(ans.back() != a[i]) ans.push_back(a[i]);
-                i++;
             }
-             else if(a[i] > b[j] ){
-             if(ans.back() != b[j])    ans.push_back(b[j]);
-                j++;
-            }
-           
-           
+            else j++;
         }
-         while(i < n1 ){
-             if(ans.back() != a[i])  ans.push_back(a[i]);
-            i++;
-        }
-        while(j < n2 ){
-            if(ans.back() != b[j])  ans.push_back(b[j]);
-            j++;
-        }
-       
         return ans;
     }
 };
