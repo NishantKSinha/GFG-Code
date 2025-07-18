f0a9156eb59f7c2329ea616519dc456a9959f036
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-07-18 10:14:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.cpp
+++ Difficulty: Medium/Prefix to Infix Conversion/prefix-to-infix-conversion.cpp
@@ -1,61 +1,22 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 
 class Solution {
   public:
     string preToInfix(string exp) {
         // Write your code here
-         int i = exp.length()-1 ;
-        stack<string>st;
-        
-        while(i >=0){
-            
-             if((exp[i] >= 'A' && exp[i] <= 'Z') || (exp[i] >= 'a' && exp[i] <= 'z') ){
-                
-                string oper(1 , exp[i]);//convert char to striing
-                st.push(oper);
+            stack<string>st;
+        for(int i = exp.size()-1  ; i >= 0 ;i--){
+            char ch = exp[i];
+            if(ch>= 'A' && ch >= 'Z' || ch>= 'a' && ch >= 'z' || ch>= '1' && ch >= '9'){
+                st.push(string(1, ch));
             }
-            else{ //for operator
-                string t1 = st.top();
-                st.pop();
-                string t2 = st.top();
-                st.pop();
-                
-                
-                st.push("(" + t1 + exp[i] + t2 + ")");
+            else{
+                string t1 = st.top(); st.pop();
+                string t2 = st.top(); st.pop();
+                string t3 = '(' + t1 + ch + t2 + ')';
+                st.push(t3);
             }
-            i--;
         }
-        return st.top();
+        return st.top(); 
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t = 1;
-    cin >> t;
-
-    // freopen ("output_gfg.txt", "w", stdout);
-
-    while (t--) {
-        // Input
-        string prefix;
-        cin >> prefix;
-
-        Solution obj;
-        cout << obj.preToInfix(prefix) << endl;
-
-        // cout << "~\n";
-    }
-    // fclose(stdout);
-
-    return 0;
-}
-
-// } Driver Code Ends
