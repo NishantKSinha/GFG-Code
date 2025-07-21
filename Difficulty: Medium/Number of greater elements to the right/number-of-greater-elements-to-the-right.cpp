6877f73c06dd0be0cd33b134c5b0ac4848905581
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-07-21 10:22:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of greater elements to the right/number-of-greater-elements-to-the-right.cpp
+++ Difficulty: Medium/Number of greater elements to the right/number-of-greater-elements-to-the-right.cpp
@@ -1,67 +1,19 @@
-//{ Driver Code Starts
-//Initial Template for C++
-#include<bits/stdc++.h>
-using namespace std;
+// User function Template for C++
 
+class Solution {
+  public:
 
-// } Driver Code Ends
-//User function Template for C++
-
-class Solution{
-public:
-
-    vector<int> count_NGE(int n, vector<int> &arr, int queries, vector<int> &indices){
-        //write your code here
-     //tc = 
-    vector<int> result;
-
-    for (int j = 0 ; j <queries;j++) {
-        int count = 0;
-        for (int i = indices[j] + 1; i < n; ++i) {
-            if (arr[i] > arr[indices[j]]) {
-                ++count;
+    vector<int> count_NGE(int n, vector<int> &arr, int queries, vector<int> &indices) {
+        // write your code here
+        vector<int>ans(queries);
+        for(int i =0 ; i < indices.size();i++){
+            int idx = indices[i];
+            stack<int>st;
+            for(int j = idx+1 ; j < n; j++){
+                if(arr[j] > arr[idx]) st.push(arr[j]);
             }
+            ans[i] = st.size();
         }
-        result.push_back(count);
+        return ans;
     }
-
-    return result;
-       
-    }
-
 };
-
-//{ Driver Code Starts.
-int main(){
-    int t = 1;
-    cin >> t;
-
-    // freopen ("output_gfg.txt", "w", stdout);
-
-    while(t--){
-        //Input
-
-       int n; cin >> n;
-       vector<int> v1(n);
-       for(int i = 0;i<n;i++) cin >> v1[i];
-        int q; cin >> q;
-        vector<int> v2(q);
-        for(int i = 0;i<q;i++) cin >> v2[i];
-
-        Solution obj;
-        vector<int> ans = obj.count_NGE(n,v1,q,v2);
-
-        for(int i = 0;i<ans.size();i++)
-            cout << ans[i] << " ";
-        cout << endl;
-
-        
-        // cout << "~\n";
-    
-cout << "~" << "\n";
-}
-    // fclose(stdout);
-
-    return 0;
-}
-// } Driver Code Ends
