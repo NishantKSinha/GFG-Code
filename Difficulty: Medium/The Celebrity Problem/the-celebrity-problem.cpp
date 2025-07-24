bd432dea1b3fafb0ebc31d1e354b7dd2675fdcd4
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-07-24 10:44:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,57 +1,27 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
-    // Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& mat) {
         // code here
-        int n = mat.size();
-        vector<int>knowme(n);
-        vector<int>iknow(n);
-        
-        for(int i = 0 ; i< n ; i++){
-            for(int j = 0 ; j < n  ; j++){
-                
-                if(mat[i][j] ==1){
-                    knowme[j]++;
-                    iknow[i]++;
-                }
+        int ans =-1;
+        int cnt1 =0;
+        for(int j =0 ;j < mat[0].size();j++){
+            int cnt =0;
+            for(int i = 0 ;i < mat.size();i++){
+                if(mat[i][j] == 1) cnt++;
             }
+             if(cnt == mat.size()){
+                     cnt1++;
+                     ans =j;
+                }
         }
         
-        for(int i = 0 ; i < n ; i++){
-            if(knowme[i] == n-1 && iknow[i] == 0) return i ;
+        if(cnt1 == 1) {
+            int cnt =0;
+            for(int j = 0 ; j < mat[0].size();j++){
+                if(mat[ans][j] == 1) cnt++;
+            }
+            if(cnt == 1) return ans ;
         }
         return -1;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int> > M(n, vector<int>(n, 0));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                cin >> M[i][j];
-            }
-        }
-        Solution ob;
-        cout << ob.celebrity(M) << endl;
-        cout << "~" << endl;
-    }
-}
-
-// } Driver Code Ends
