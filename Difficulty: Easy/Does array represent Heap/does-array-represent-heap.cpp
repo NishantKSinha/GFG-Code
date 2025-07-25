ad0fc8495b100514db793a64df87f4b312e8c3a6
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-07-25 11:37:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Does array represent Heap/does-array-represent-heap.cpp
+++ Difficulty: Easy/Does array represent Heap/does-array-represent-heap.cpp
@@ -1,54 +1,15 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
 
-// } Driver Code Ends
-
-class Solution{
-    public:
-    bool isMaxHeap(int arr[], int n)
-    {
+class Solution {
+  public:
+    bool isMaxHeap(int arr[], int n) {
         // Your code goes here
-        // Traverse all internal nodes
-        for (int i = 0; i <= (n - 2) / 2; i++) {
-            int left = 2 * i + 1;  // Index of the left child
-            int right = 2 * i + 2; // Index of the right child
-
-            // Check if the left child exists and violates the Max Heap property
-            if (left < n && arr[i] < arr[left]) {
-                return false;
-            }
-
-            // Check if the right child exists and violates the Max Heap property
-            if (right < n && arr[i] < arr[right]) {
-                return false;
+        for(int i =0; i <n ;i++){
+            int left = 2*i +1;
+            int right = 2*i + 2;
+            if(left < n && arr[left] > arr[i] || right < n && arr[right] > arr[i] ){
+                return 0;
             }
         }
-        // If no violations, it is a Max Heap
-        return true;
+        return 1;
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    
-    int t;
-    cin >> t;
-    while(t--)
-    {
-       int n;
-       cin >> n;
-       int a[4*n]={0};
-       for(int i =0;i<n;i++){
-           cin >> a[i];
-       }
-       Solution ob;
-       cout<<ob.isMaxHeap(a, n)<<endl;
-        
-    
-cout << "~" << "\n";
-}
-    return 0;
-}
-
-// } Driver Code Ends
