bb9d27a68ed95f4ac67f2ce4bc881a8be169f368
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-08-16 20:05:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,68 +1,29 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-  // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(vector<vector<int>> &adj) {
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfs(vector<vector<int>> &adj) {
         // Code here
-        //0-base indexing so we take vis[n] , if its 1-base take vis[n+1]
-        int vis[adj.size()] = {0};
-        // initially we always mark first node i.e 0 as visited and push into queue
-        vis[0] = 1;
+        vector<int>ans;
         queue<int>q;
-        q.push(0);
+        vector<int>vis(adj.size(),0);
         
-        vector<int> bfs;
+        q.push(0);
+        vis[0] = 1;
         
         while(!q.empty()){
-            int node = q.front();
+            int val = q.front();
+            ans.push_back(val);
+           
             q.pop();
-            bfs.push_back(node);
             
-            //now we will check node neighbour from adjacency list
-            
-            for(auto it : adj[node]){
-                if(!vis[it]){
-                    vis[it] = 1;
+            for(auto it : adj[val]){
+                
+                if(vis[it] != 1){
                     q.push(it);
+                    vis[it] = 1;
                 }
             }
         }
-        return bfs;
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
-        // Now using vector of vectors instead of array of vectors
-        vector<vector<int>> adj(V);
-
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u); // For undirected graph, add both u->v and v->u
-        }
-
-        Solution obj;
-        vector<int> ans = obj.bfsOfGraph(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
