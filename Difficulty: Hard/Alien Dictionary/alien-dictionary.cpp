191a2df67504c95eb15ca0eb3c4849ce74b73d58
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-08-28 20:19:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
@@ -1,132 +1,64 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
-// User function Template for C++
-
 class Solution {
-    private:
-    vector<int> topoSort(int V, vector<int> adj[])
-	{
-		int indegree[V] = {0};
-		for (int i = 0; i < V; i++) {//traversing in adjacency matrix
-			for (auto it : adj[i]) {//traversing in adjacency node
-				indegree[it]++;               //for outdegree===outdegree[i]++;
-			}
-		}
-
-		queue<int> q;
-		for (int i = 0; i < V; i++) {
-			if (indegree[i] == 0) {
-				q.push(i);
-			}
-		}
-		vector<int> topo;
-		while (!q.empty()) {
-			int node = q.front();
-			q.pop();
-			topo.push_back(node);
-			// node is in your topo sort
-			// so please remove it from the indegree
-
-			for (auto it : adj[node]) {
-				indegree[it]--;
-				if (indegree[it] == 0) q.push(it);
-			}
-		}
-
-		return topo;
-	}
   public:
-    string findOrder(vector<string> dict, int k) {
-        // code here
-        int N = dict.size();//here k is count of differnt type of leeter use
-        
-        vector<int>adj[k];
-        
-        for(int i = 0;i<N-1;i++){
-            string s1 = dict[i];
-            string s2 = dict[i+1];
+   void bfs(vector<int>& indegree, vector<vector<int>>& adj, queue<int>& q, vector<int>& ans){
+        while(!q.empty()){
+            int val = q.front();
+            ans.push_back(val);
+            q.pop();
             
-            int len = min(s1.length(),s2.length());
+            for(auto it: adj[val]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+    }
+
+    string findOrder(vector<string> &words) {
+        unordered_set<char> unique_letters;
+        for (auto &word : words) {
+            for (char c : word) {
+                unique_letters.insert(c);
+            }
+        }
+        
+        vector<vector<int>> adj(26);
+        vector<int> indegree(26,0);
+        
+        // build graph
+        for(int i=0 ; i < (int)words.size()-1;i++){
+            string str1 = words[i];
+            string str2 = words[i+1];
             
-            for(int pointr = 0 ;pointr<len;pointr++){
-                if(s1[pointr] != s2[pointr]){
-                    adj[s1[pointr]-'a'].push_back(s2[pointr]-'a');
+            // prefix check: invalid case
+            if(str1.size() > str2.size() && str1.substr(0, str2.size()) == str2)
+                return "";  // invalid dictionary
+            
+            int len = min(str1.size(), str2.size());
+            for(int j=0 ; j < len;j++){
+                if(str1[j] != str2[j]) {
+                    adj[str1[j]-'a'].push_back(str2[j]-'a');
+                    indegree[str2[j]-'a']++;
                     break;
                 }
             }
         }
-        vector<int>topo = topoSort(k,adj); //we will pass adj bcz it indicate directed graph
-        string ans="";
-        for(auto it:topo){
-            ans += (it+'a');
+        
+        queue<int> q;
+        for(char c : unique_letters){
+            if(indegree[c - 'a'] == 0) q.push(c - 'a');
         }
-        return ans;
+
+        vector<int> ans;
+        bfs(indegree, adj, q, ans);
+       
+        // check if all characters are covered (no cycle)
+        if(ans.size() != unique_letters.size()) 
+            return "";
+        
+        string res ="";
+        for(int i =0 ; i < ans.size();i++){
+            res += char('a' + ans[i]);  
+        }
+        return res;
     }
 };
-
-//{ Driver Code Starts.
-string order;
-
-bool f(string a, string b) {
-    int p1 = 0;
-    int p2 = 0;
-    for (int i = 0; i < min(a.size(), b.size()) and p1 == p2; i++) {
-        p1 = order.find(a[i]);
-        p2 = order.find(b[i]);
-        //	cout<<p1<<" "<<p2<<endl;
-    }
-
-    if (p1 == p2 and a.size() != b.size())
-        return a.size() < b.size();
-
-    return p1 < p2;
-}
-
-// Driver program to test above functions
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<string> a;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        string number;
-        while (ss >> number) {
-            a.push_back(number);
-        }
-        int K;
-        cin >> K;
-        getchar();
-        Solution obj;
-        string ans = obj.findOrder(a, K);
-        order = "";
-        for (int i = 0; i < ans.size(); i++)
-            order += ans[i];
-
-        string temp[a.size()];
-        std::copy(a.begin(), a.end(), temp);
-        sort(temp, temp + a.size(), f);
-
-        bool f = true;
-        for (int i = 0; i < a.size(); i++)
-            if (a[i] != temp[i])
-                f = false;
-
-        if (f)
-            cout << "true";
-        else
-            cout << "false";
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
