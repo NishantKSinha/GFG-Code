5ffe38796d6ab8228703ba7532eb634313d7e064
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-08-29 21:48:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -1,107 +1,39 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 class Solution {
-     private:
-    void topoSort(int node, vector < pair < int, int >> adj[],
-      int vis[], stack < int > & st) {
-      //This is the function to implement Topological sort. 
-      vis[node] = 1;
-      for (auto it: adj[node]) {
-        int v = it.first;
-        if (!vis[v]) {
-          topoSort(v, adj, vis, st);
-        }
-      }
-      st.push(node);
-    }
   public:
     vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
-        // code here
-        //We create a graph first in the form of an adjacency list.
-      vector < pair < int, int >> adj[V];
-      for (int i = 0; i < E; i++) {
-        int u = edges[i][0];
-        int v = edges[i][1];
-        int wt = edges[i][2];
-        adj[u].push_back({v, wt}); 
-      }
-      // A visited array is created with initially 
-      // all the nodes marked as unvisited (0).
-      int vis[V] = {0};
-      //Now, we perform topo sort using DFS technique 
-      //and store the result in the stack st.
-      stack < int > st;
-      for (int i = 0; i < V; i++) {
-        if (!vis[i]) {
-          topoSort(i, adj, vis, st);
+        
+        vector<vector<pair<int,int>>> adj(V);  // n empty adjacency lists
+
+         
+        for(auto it: edges){
+            adj[it[0]].push_back({it[1],it[2]});
         }
-      }
-      //Further, we declare a vector ‘dist’ in which we update the value of the nodes’
-      //distance from the source vertex after relaxation of a particular node.
-
-      vector < int > dist(V);
-      for (int i = 0; i < V; i++) {
-        dist[i] = 1e9;
-      }
-
-      dist[0] = 0;//DIST[SRC]=0
-      while (!st.empty()) {
-        int node = st.top();
-        st.pop();
-
-        for (auto it: adj[node]) {
-          int v = it.first;
-          int wt = it.second;
-
-          if (dist[node] + wt < dist[v]) {
-            dist[v] = wt + dist[node];
-          }
+        
+         queue<pair<int,int>>q;
+         vector<int>ans(adj.size(),INT_MAX);
+        q.push({0,0});
+        ans[0] =0;
+        
+        while(!q.empty()){
+            int dist = q.front().second;
+            int val = q.front().first;
+            q.pop();
+            
+            for(auto it: adj[val]){
+                if(ans[it.first] > dist+it.second){
+                    q.push({it.first,dist+it.second});
+                    ans[it.first] = dist+it.second;
+                }
+            }
+            
         }
-      }
-
-      for (int i = 0; i < V; i++) {
-        if (dist[i] == 1e9) dist[i] = -1;
-      }
-      return dist;
+        
+        for(int i=0 ; i < ans.size();i++){
+            if(ans[i] == INT_MAX) ans[i] =-1;
+        }
+        
+        return ans;
+        
     }
 };
-
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> edges;
-        for (int i = 0; i < m; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 3; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            edges.push_back(temp);
-        }
-        Solution obj;
-        vector<int> res = obj.shortestPath(n, m, edges);
-        for (auto x : res) {
-            cout << x << " ";
-        }
-        cout << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-}
-
-// } Driver Code Ends
