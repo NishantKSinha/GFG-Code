667f019d92aec9adba65c53929afdcc607bec062
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-09-13 18:17:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,111 +1,32 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-
-class DisjointSet {
-   
-public:
- vector<int> parent, size;
-    DisjointSet(int n) {
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    int findUPar(int node) {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-};
-
-
 class Solution {
   public:
-    // Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[]) {
+    int spanningTree(int V, vector<vector<int>>& edges) {
         // code here
-        vector<pair<int, pair<int, int>>> edges;  // vector<pair<int, int>> mst; for storing
-        for (int i = 0; i < V; i++) {
-            for (auto it : adj[i]) {
-                int adjNode = it[0];
-                int wt = it[1];
-                int node = i;
-
-                edges.push_back({wt, {node, adjNode}});
-            }
+        vector<vector<pair<int,int>>>adj(V);
+        for(auto it :edges){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
         }
-        DisjointSet ds(V);
-        sort(edges.begin(), edges.end());
-        int mstWt = 0;
-        for (auto it : edges) {
-            int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
-
-            if (ds.findUPar(u) != ds.findUPar(v)) {
-                // mst.push_back({u, v});   and declare mst vector above for loop
-                mstWt += wt;
-                ds.unionBySize(u, v);
-            }
-        }
-
-        return mstWt;
+        
+        vector<int>vis(V,0);
+       priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+       
+       pq.push({0,0});
+       int sum =0;
+       
+       while(!pq.empty()){
+           int dist = pq.top().first;
+           int node = pq.top().second;
+           pq.pop();
+           if(vis[node] == 0){
+               vis[node] = 1;
+               sum = sum + dist;
+           }
+           
+           for(auto it : adj[node]){
+               if(vis[it.first] == 0 ) pq.push({it.second , it.first});
+           }
+       }
+       return sum;
     }
 };
-    
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> adj[V];
-        int i = 0;
-        while (i++ < E) {
-            int u, v, w;
-            cin >> u >> v >> w;
-            vector<int> t1, t2;
-            t1.push_back(v);
-            t1.push_back(w);
-            adj[u].push_back(t1);
-            t2.push_back(u);
-            t2.push_back(w);
-            adj[v].push_back(t2);
-        }
-
-        Solution obj;
-        cout << obj.spanningTree(V, adj) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
