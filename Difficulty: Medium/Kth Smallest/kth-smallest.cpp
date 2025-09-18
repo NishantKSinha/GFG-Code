e1345089f60cb33d32f5fb97db9597b7207cafee
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-09-18 23:46:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -1,11 +1,3 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function template for C++
 
 class Solution {
@@ -14,47 +6,18 @@ class Solution {
     // k : find kth smallest element and return using this function
     int kthSmallest(vector<int> &arr, int k) {
         // code here
-         priority_queue<int,vector<int>,greater<int>>pq(arr.begin(),arr.end());
-        int ans = 0 ;
-        while(k!=0){
-            ans=pq.top();
-            pq.pop();
-            k--;
-
+        priority_queue<int,vector<int>,greater<int>>pq;
+        
+        for(int i=0; i < arr.size();i++){
+            pq.push(arr[i]);
         }
-        return ans;
+        
+        int cnt = k-1;
+        while(cnt != 0) {
+            pq.pop();
+            cnt--;
+        }
+        
+        return pq.top();
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int k;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        k = crr[0];
-        int n = arr.size();
-        Solution ob;
-        cout << ob.kthSmallest(arr, k) << endl << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
