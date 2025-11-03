cc560598700c690e0557e80061af8467e162fd66
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-11-03 20:49:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,90 +1,33 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-  bool countStudents(vector<int> &arr, int pages,int k ) {
-    int n = arr.size(); //size of array.
-    int students = 1;
-    long long pagesStudent = 0;
-    for (int i = 0; i < n; i++) {
-        if (pagesStudent + arr[i] <= pages) {
-            //add pages to current student
-            pagesStudent += arr[i];
-        }
-        else {
-            //add pages to next student
-            students++;
-            pagesStudent = arr[i];
-        }
-    }
-    if(students <= k) return true;
-    else return false;
-}
-  
     int findPages(vector<int> &arr, int k) {
         // code here
-        int n = arr.size();
-        int m = k;
-        if (m > n) return -1;
+        int h =accumulate(arr.begin(), arr.end(), 0);
+        if(arr.size() < k) return -1;
+        int l = *max_element(arr.begin(), arr.end());
 
-    int low = *max_element(arr.begin(), arr.end());
-    int high = accumulate(arr.begin(), arr.end(), 0);
-     int ans =-1; 
-    while (low <= high) {
-        int mid = (low + high) / 2;
+        int ans = INT_MAX;
+        while(l <= h){
+            int mid= (l+h)/2;
+            int sum=0;
+            int cnt =1;
+            int i=0;
+            while( i < arr.size()){
+                sum += arr[i];
+                if(sum > mid){
+                    cnt++;
+                    sum =arr[i];
+                    i++;
+                }
+                else i++;
+            }
+            if(cnt <= k ){
+                ans = min(ans,mid);
+                h= mid-1;
+            }
+            else l =mid+1;
+        }
+        return ans;
         
-        if ( countStudents(arr, mid,k) == true) {
-            ans = mid;
-            high = mid -1 ;
-        }
-        else {
-            low = mid +1 ;
-        }
-    }
-    return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int d;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        d = crr[0];
-        int n = arr.size();
-        Solution ob;
-        int ans = ob.findPages(arr, d);
-        cout << ans << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
