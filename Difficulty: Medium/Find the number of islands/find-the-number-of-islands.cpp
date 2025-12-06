60ce9b196b487df62aed5257342f3e2dbf3ede98
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-12-06 10:45:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.cpp
@@ -1,40 +1,40 @@
 class Solution {
   public:
-    void dfs(vector<vector<char>>& grid, vector<vector<int>>&vis,int i , int j){
-        vis[i][j] =1;
-        int n= grid.size();
-        int m= grid[0].size();
-        
-       int dr[] = {-1, +1, 0, 0, -1, -1, +1, +1};
-       int dc[] = {0, 0, -1, +1, -1, +1, -1, +1};
-       
-       for(int k=0 ;k <8;k++){
-           int newr = dr[k] +i;
-           int newc = dc[k] + j;
-           
-           if(newr >=0 && newc >=0 && newr <n && newc < m && vis[newr][newc] == 0 && grid[newr][newc] == 'L'){
-               dfs(grid,vis,newr,newc);
-           }
-       }
-    }
+    void  bfs(vector<vector<int>>&vis,int r ,int c , vector<vector<char>>&grid){
+      queue<pair<int,int>>dq;
+      dq.push({r,c});
+      int dr[] = {0,0,-1,1,-1, -1, +1, +1};
+      int dc[] = {-1,1,0,0,-1,1,-1,1};
+      vis[r][c] = 1;
+      while(!dq.empty()){
+          int kr = dq.front().first;
+          int kc = dq.front().second;
+          dq.pop();
+          for(int k = 0 ; k <8;k++){
+            int nr = kr + dr[k];
+            int nc = kc + dc[k];
+            if(nr >= 0 && nc >=0 && nr <grid.size() && nc < grid[0].size() && vis[nr][nc] ==0 && grid[nr][nc] == 'L'){
+                 vis[nr][nc] =1;
+               
+                 dq.push({nr,nc});
+            }
+          }
+      }
+   }
     int countIslands(vector<vector<char>>& grid) {
         // Code here
+         vector<vector<int>>vis(grid.size(),vector<int>(grid[0].size(),0));
         int cnt =0;
-        int n= grid.size();
-        int m= grid[0].size();
-        
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        
-        for(int i=0 ;i <n;i++){
-            for(int j =0 ; j <m ;j++){
-                if(grid[i][j] == 'W') vis[i][j] =1;
+        for(int i =0 ; i < grid.size();i++){
+            for(int j=0 ; j < grid[0].size();j++){
                 
-                else if(grid[i][j] == 'L' && vis[i][j] == 0){
-                    cnt++;
-                    dfs(grid,vis,i,j);
+                    if(grid[i][j] == 'L' && vis[i][j] == 0){
+                        cnt++;
+                        bfs(vis,i,j,grid);
+                    }
                 }
             }
-        }
+        
         return cnt;
     }
 };
