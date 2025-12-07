a059e900a3c11a0ac3c3b51b540a0063d68b32a2
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-12-07 10:14:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -1,39 +1,34 @@
 // User function Template for C++
 class Solution {
   public:
-    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
-        
-        vector<vector<pair<int,int>>> adj(V);  // n empty adjacency lists
+   void bfs(  vector<vector<pair<int,int>>>adj,  vector<int>&ans,int src){
+       priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
 
-         
-        for(auto it: edges){
+       pq.push({0,src});
+       ans[src] = 0;
+       while(!pq.empty()){
+           int node = pq.top().second;
+           int dist = pq.top().first;
+           pq.pop();
+           for(auto it: adj[node]){
+               if(ans[it.first] > ans[node] + it.second){
+                   pq.push({ans[node]+it.second,it.first});
+                   ans[it.first] = ans[node]+it.second;
+               }
+           }
+       }
+   }
+    vector<int> shortestPath(int V, int E, vector<vector<int>>& edges) {
+        // code here
+          vector<int>ans(V,INT_MAX);
+        vector<vector<pair<int,int>>>adj(V);
+        for(auto it : edges){
             adj[it[0]].push_back({it[1],it[2]});
         }
-        
-         queue<pair<int,int>>q;
-         vector<int>ans(adj.size(),INT_MAX);
-        q.push({0,0});
-        ans[0] =0;
-        
-        while(!q.empty()){
-            int dist = q.front().second;
-            int val = q.front().first;
-            q.pop();
-            
-            for(auto it: adj[val]){
-                if(ans[it.first] > dist+it.second){
-                    q.push({it.first,dist+it.second});
-                    ans[it.first] = dist+it.second;
-                }
-            }
-            
-        }
-        
-        for(int i=0 ; i < ans.size();i++){
+        bfs(adj,ans,0);
+        for(int i=0;i < V; i++){
             if(ans[i] == INT_MAX) ans[i] =-1;
         }
-        
         return ans;
-        
     }
 };
