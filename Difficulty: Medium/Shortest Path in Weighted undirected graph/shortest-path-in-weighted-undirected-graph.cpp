df5b1c6691c08d08feca5be802f049d1620d4c35
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-12-07 11:05:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Weighted undirected graph/shortest-path-in-weighted-undirected-graph.cpp
+++ Difficulty: Medium/Shortest Path in Weighted undirected graph/shortest-path-in-weighted-undirected-graph.cpp
@@ -1,49 +1,42 @@
 class Solution {
   public:
+     void bfs(vector<vector<pair<int,int>>>adj,  vector<int>&ans,int src,vector<int>&parent){
+       priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+       pq.push({0,src});
+       ans[src] = 0;
+       while(!pq.empty()){
+           int node = pq.top().second;
+           int dist = pq.top().first;
+           pq.pop();
+           for(auto it: adj[node]){
+               if(ans[it.first] > ans[node] + it.second){
+                   pq.push({ans[node]+it.second,it.first});
+                   ans[it.first] = ans[node]+it.second;
+                   parent[it.first] = node;
+               }
+           }
+       }
+   }
     vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
         // Code here
-         vector<vector<pair<int,int>>>adj(n+1);  //1-base indexing
-         
+          vector<int>ans(n+1,INT_MAX);
+             vector<int>parent(n+1,-1);
+        vector<vector<pair<int,int>>>adj(n+1);
         for(auto it : edges){
             adj[it[0]].push_back({it[1],it[2]});
             adj[it[1]].push_back({it[0],it[2]});
         }
-        
-        vector<int>distance(n+1,INT_MAX);
-        vector<int>parent(n+1,-1);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-         
-        pq.push({0,1});  //dist,node
-        distance[1] =0;
-        
-        while(!pq.empty()){
-            int dist = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-            
-            for(auto it: adj[node]){
-                if(it.second+dist < distance[it.first]){
-                    distance[it.first] = it.second+dist;
-                    parent[it.first] = node;
-                    pq.push({it.second+dist,it.first});
-                }
-            }
-        }
-        
-         if(distance[n] == INT_MAX) return {-1}; //if destination not reachable
-        
-        vector<int>ans;
-         
-        int i = n;
-        while(i != -1){
-            ans.push_back(i);
-            i = parent[i];
-        }
-        
-        reverse(ans.begin(),ans.end());
-        
-        // insert distance at the front
-ans.insert(ans.begin(), distance[n]);
-        return ans;
+        bfs(adj,ans,1,parent);
+         if(ans[n] == INT_MAX) return {-1};
+       vector<int>res;
+       int i = n;
+       while(i != -1){
+           res.push_back(i);
+           i = parent[i];
+       }
+       reverse(res.begin(),res.end());
+       res.insert(res.begin(),ans[n]);
+       return res;
     }
 };
