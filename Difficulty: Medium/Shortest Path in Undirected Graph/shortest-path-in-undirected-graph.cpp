321f8c3cfbbc19e31e11695d820d83cc4dc86be8
Nishant Kumar Sinha <nishantsinha233@gmail.com>
2025-12-07 10:04:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.cpp
+++ Difficulty: Medium/Shortest Path in Undirected Graph/shortest-path-in-undirected-graph.cpp
@@ -1,32 +1,36 @@
 class Solution {
   public:
-    // Function to find the shortest path from source to all other nodes
-    vector<int> shortestPath(vector<vector<int>>& adj, int src) {
+   void bfs( vector<vector<int>>adj,  vector<int>&ans,int src){
+       priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+       pq.push({0,src});
+       ans[src] = 0;
+       while(!pq.empty()){
+           int node = pq.top().second;
+           int dist = pq.top().first;
+           pq.pop();
+           for(auto it: adj[node]){
+               if(ans[it] > ans[node] + 1){
+                   pq.push({ans[node]+1,it});
+                   ans[it] = ans[node]+1;
+               }
+           }
+       }
+   }
+    vector<int> shortestPath(int V, vector<vector<int>> &edges, int src) {
         // code here
-        vector<int>ans(adj.size(),INT_MAX);
-        queue<pair<int,int>>q;
         
-        q.push({src,0});
-        ans[src] =0;
         
-        while(!q.empty()){
-            int dist = q.front().second;
-            int val = q.front().first;
-            q.pop();
-            
-            for(auto it: adj[val]){
-                if(ans[it] > dist+1){
-                    q.push({it,dist+1});
-                    ans[it] = dist+1;
-                }
-            }
-            
+        vector<int>ans(V,INT_MAX);
+        vector<vector<int>>adj(V);
+        for(auto it : edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
-        
-        for(int i=0 ; i < ans.size();i++){
+        bfs(adj,ans,src);
+        for(int i=0;i < V; i++){
             if(ans[i] == INT_MAX) ans[i] =-1;
         }
-        
         return ans;
     }
 };
